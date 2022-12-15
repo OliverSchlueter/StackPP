cbfb78d996147b28e18642a7d85c690eba93aea7
OliverSchlueter <oliverhd.1303@gmail.com>
2022-12-15 17:52:55 +0100

    Add macros

--- src/main/java/de/oliver/stackpp/Lexer.java
+++ src/main/java/de/oliver/stackpp/Lexer.java
@@ -6,12 +6,14 @@ public class Lexer {
 
     private final String content;
     private final String[] lines;
+    private final Map<String, String> macros;
 
     private LinkedList<Instruction> instructions;
 
     public Lexer(String content) {
         this.content = content.strip();
         this.lines = this.content.split("\r\n");
+        this.macros = new HashMap<>();
         this.instructions = new LinkedList<>();
     }
 
@@ -36,9 +38,32 @@ public class Lexer {
                 line = line.substring(0, line.indexOf(";"));
             }
 
+            for (String macroKey : macros.keySet()) {
+                String replacement = macros.get(macroKey);
+
+                line = line.replace(macroKey, replacement);
+            }
+
             String[] words = line.strip().split("\\s+");
 
             if(words.length == 0){
+                continue;
+            }
+
+            if(words[0].equalsIgnoreCase("#define")){
+                if(words.length < 3){
+                    continue;
+                }
+
+                String replacement = "";
+
+                for (int j = 2; j < words.length; j++) {
+                    replacement += words[j] + " ";
+                }
+
+                replacement = replacement.substring(0, replacement.length()-1);
+
+                macros.put(words[1], replacement);
                 continue;
             }
 
@@ -75,5 +100,9 @@ public class Lexer {
 
     public LinkedList<Instruction> getInstructions() {
         return instructions;
+    }
+
+    public Map<String, String> getMacros() {
+        return macros;
     }
 }
