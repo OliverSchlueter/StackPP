026d5a86d95ca50c1a904ff75038ed247b938d7b
OliverSchlueter <oliverhd.1303@gmail.com>
2022-12-21 16:36:10 +0100

    Add tests for memget and memset

---
+++ src/test/java/de/oliver/stackpp/operations/impl/memory/MemoryGetOperationTest.java
@@ -0,0 +1,28 @@
+package de.oliver.stackpp.operations.impl.memory;
+
+import de.oliver.stackpp.Program;
+import de.oliver.stackpp.operations.Operation;
+import de.oliver.stackpp.virtualMachine.Machine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryGetOperationTest {
+
+    private Program program;
+
+    @BeforeEach
+    void setUp() {
+        program = new Program(new Machine());
+        program.getMachine().getMemory().setAt(3, (byte) 71);
+    }
+
+    @Test
+    void execute() {
+        Operation operation = new MemoryGetOperation(program, p -> 3);
+        operation.execute();
+
+        assert program.getMachine().getStack().pop() == 71;
+    }
+}
---
+++ src/test/java/de/oliver/stackpp/operations/impl/memory/MemorySetOperationTest.java
@@ -0,0 +1,27 @@
+package de.oliver.stackpp.operations.impl.memory;
+
+import de.oliver.stackpp.Program;
+import de.oliver.stackpp.operations.Operation;
+import de.oliver.stackpp.virtualMachine.Machine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemorySetOperationTest {
+
+    private Program program;
+
+    @BeforeEach
+    void setUp(){
+        program = new Program(new Machine());
+    }
+
+    @Test
+    void execute() {
+        Operation operation = new MemorySetOperation(program, p -> 7, p -> (byte)42);
+        operation.execute();
+
+        assert program.getMachine().getMemory().getAt(7) == 42;
+    }
+}
