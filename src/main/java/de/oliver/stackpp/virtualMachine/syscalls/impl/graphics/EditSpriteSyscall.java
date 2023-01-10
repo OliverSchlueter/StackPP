06506bf96da861f19bdf6c88148d74ef62006180
Oliver <79666085+OliverSchlueter@users.noreply.github.com>
2023-01-10 11:45:03 +0100

    Add edit sprite color

--- src/main/java/de/oliver/stackpp/virtualMachine/syscalls/impl/graphics/EditSpriteSyscall.java
+++ src/main/java/de/oliver/stackpp/virtualMachine/syscalls/impl/graphics/EditSpriteSyscall.java
@@ -5,6 +5,7 @@ import de.oliver.gameEngine.components.SpriteComponent;
 import de.oliver.stackpp.Program;
 import de.oliver.stackpp.virtualMachine.Machine;
 import de.oliver.stackpp.virtualMachine.syscalls.Syscall;
+import org.joml.Vector4f;
 
 public class EditSpriteSyscall extends Syscall {
 
@@ -27,8 +28,25 @@ public class EditSpriteSyscall extends Syscall {
             case Y_POS -> gameObject.transform.position.y = val;
             case WIDTH -> gameObject.transform.scale.x = val;
             case HEIGHT -> gameObject.transform.scale.y = val;
+            case COLOR -> sprite.setColor(colorFromInt(val));
         }
+    }
 
+    private static Vector4f colorFromInt(int color){
+        float a = (float) ((color >> 24) & 0xff) / 255.0f;
+        float r = (float) ((color >> 16) & 0xff) / 255.0f;
+        float g = (float) ((color >> 8)  & 0xff) / 255.0f;
+        float b = (float) ((color >> 0)  & 0xff) / 255.0f;
+
+        return new Vector4f(r, g, b, a);
+    }
+
+    private static int colorToInt(int r, int g, int b, int a){
+        int color = ((int) (a * 255.0f) << 24) |
+                    ((int) (r * 255.0f) << 16) |
+                    ((int) (g * 255.0f) << 8)  |
+                    ((int) (b * 255.0f) << 0);
+        return color;
     }
 }
 
@@ -37,6 +55,7 @@ enum Action{
     Y_POS(1),
     WIDTH(2),
     HEIGHT(3),
+    COLOR(4)
     ;
 
     private final int actionId;
