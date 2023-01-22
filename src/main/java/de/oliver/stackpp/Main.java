e98b8cad109bedcdd2f9a3f829e45808ae9c5b23
OliverSchlueter <oliverhd.1303@gmail.com>
2023-01-22 19:24:10 +0100

    Add sort lib

--- src/main/java/de/oliver/stackpp/Main.java
+++ src/main/java/de/oliver/stackpp/Main.java
@@ -18,7 +18,7 @@ public class Main {
         } else {
             content = String.join("\r\n",
                     "",
-                    "#INCLUDE math.spp",
+                    "#INCLUDE sort.spp",
                     ""
                     );
         }
