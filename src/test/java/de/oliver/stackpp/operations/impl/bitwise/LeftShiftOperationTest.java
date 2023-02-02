b98d3f1917d83a68406873e3c4b01ecaf5badc93
Oliver <79666085+OliverSchlueter@users.noreply.github.com>
2023-02-02 10:43:42 +0100

    Add unit test for 'lshift'

---
+++ src/test/java/de/oliver/stackpp/operations/impl/bitwise/LeftShiftOperationTest.java
@@ -0,0 +1,26 @@
+package de.oliver.stackpp.operations.impl.bitwise;
+
+import de.oliver.stackpp.Program;
+import de.oliver.stackpp.operations.Operation;
+import de.oliver.stackpp.virtualMachine.Machine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LeftShiftOperationTest {
+
+    private Program program;
+    @BeforeEach
+    void setUp() {
+        program = new Program(new Machine());
+        program.getMachine().getIntegerRegister("a").setValue(0b01000101);
+        program.getMachine().getIntegerRegister("b").setValue(2);
+    }
+
+    @Test
+    void execute() {
+        Operation operation = new LeftShiftOperation(program, 1, p -> p.getMachine().getIntegerRegister("a"),  p -> p.getMachine().getIntegerRegister("b").getValue());
+        operation.execute();
+
+        assert program.getMachine().getIntegerRegister("a").getValue() == 0b0100010100;
+    }
+}
